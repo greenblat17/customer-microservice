4a09d31476d81314c2311e5307ec1656d2b649d5
Alex Zhuravlev <leftemail17@mail.ru>
2024-02-11 17:01:31 +0300

    add notification queue configuration

---
+++ notification/src/main/java/com/greenblat/notification/NotificationConfig.java
@@ -0,0 +1,22 @@
+package com.greenblat.notification;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Getter
+@Setter
+public class NotificationConfig {
+
+    @Value("${rabbitmq.exchanges.internal}")
+    private String internalExchange;
+
+    @Value("${rabbitmq.queue.notification}")
+    private String notificationQueue;
+
+    @Value("${rabbitmq.routing-keys.internal-notification}")
+    private String internalNotificationRoutingKey;
+
+}
