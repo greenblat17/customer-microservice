485b66d7f9bf2a9eabaf01da8ebc6851f513f531
Alex Zhuravlev <leftemail17@mail.ru>
2024-02-13 19:45:21 +0300

    add queue topic and binding for notification

--- notification/src/main/java/com/greenblat/notification/NotificationConfig.java
+++ notification/src/main/java/com/greenblat/notification/NotificationConfig.java
@@ -2,7 +2,12 @@ package com.greenblat.notification;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -18,5 +23,23 @@ public class NotificationConfig {
 
     @Value("${rabbitmq.routing-keys.internal-notification}")
     private String internalNotificationRoutingKey;
+
+    @Bean
+    public TopicExchange internalTopicExchange() {
+        return new TopicExchange(this.internalExchange);
+    }
+
+    @Bean
+    public Queue notificationQueue() {
+        return new Queue(this.notificationQueue);
+    }
+
+    @Bean
+    public Binding internalNotificationBinding() {
+        return BindingBuilder
+                .bind(notificationQueue())
+                .to(internalTopicExchange())
+                .with(this.internalNotificationRoutingKey);
+    }
 
 }
